673bee9841ac1a2d4df480002aa5776924612ab8
vilius-stankaitis <vilius@stankaitis.com>
2022-10-06 13:32:43 +0300

    Fix Info page rendering

--- src/Home/Info/index.tsx
+++ src/Home/Info/index.tsx
@@ -1,4 +1,4 @@
-import { Main } from '@flumens';
+import { Page, Main } from '@flumens';
 import { observer } from 'mobx-react';
 import { IonIcon, IonList, IonItem } from '@ionic/react';
 import { heartOutline, informationCircleOutline } from 'ionicons/icons';
@@ -6,64 +6,66 @@ import './styles.scss';
 
 const InfoController = () => {
   return (
-    <Main className="info">
-      <div className="app-title">
-        <div>
-          <h1>
-            iRecord <span>Grasshoppers</span>
-          </h1>
-          <div className="subtitle">and related insects</div>
+    <Page id="info">
+      <Main className="info">
+        <div className="app-title">
+          <div>
+            <h1>
+              iRecord <span>Grasshoppers</span>
+            </h1>
+            <div className="subtitle">and related insects</div>
+          </div>
         </div>
-      </div>
-      <IonList lines="full">
-        <div className="rounded">
-          <IonItem routerLink="/info/about" detail>
-            <IonIcon
-              icon={informationCircleOutline}
-              size="small"
-              slot="start"
-            />
-            About this App
-          </IonItem>
-          <IonItem routerLink="/info/partners" detail>
-            <IonIcon
-              icon={informationCircleOutline}
-              size="small"
-              slot="start"
-            />
-            Partners
-          </IonItem>
-          <IonItem routerLink="/info/whereToLook" detail>
-            <IonIcon
-              icon={informationCircleOutline}
-              size="small"
-              slot="start"
-            />
-            Where to Look
-          </IonItem>
-          <IonItem routerLink="/info/furtherReading" detail>
-            <IonIcon
-              icon={informationCircleOutline}
-              size="small"
-              slot="start"
-            />
-            Further Reading
-          </IonItem>
-          <IonItem routerLink="/info/faq" detail>
-            <IonIcon
-              icon={informationCircleOutline}
-              size="small"
-              slot="start"
-            />
-            Frequently Asked Questions
-          </IonItem>
-          <IonItem routerLink="/info/credits" detail>
-            <IonIcon icon={heartOutline} size="small" slot="start" />
-            Credits
-          </IonItem>
-        </div>
-      </IonList>
-    </Main>
+        <IonList lines="full">
+          <div className="rounded">
+            <IonItem routerLink="/info/about" detail>
+              <IonIcon
+                icon={informationCircleOutline}
+                size="small"
+                slot="start"
+              />
+              About this App
+            </IonItem>
+            <IonItem routerLink="/info/partners" detail>
+              <IonIcon
+                icon={informationCircleOutline}
+                size="small"
+                slot="start"
+              />
+              Partners
+            </IonItem>
+            <IonItem routerLink="/info/whereToLook" detail>
+              <IonIcon
+                icon={informationCircleOutline}
+                size="small"
+                slot="start"
+              />
+              Where to Look
+            </IonItem>
+            <IonItem routerLink="/info/furtherReading" detail>
+              <IonIcon
+                icon={informationCircleOutline}
+                size="small"
+                slot="start"
+              />
+              Further Reading
+            </IonItem>
+            <IonItem routerLink="/info/faq" detail>
+              <IonIcon
+                icon={informationCircleOutline}
+                size="small"
+                slot="start"
+              />
+              Frequently Asked Questions
+            </IonItem>
+            <IonItem routerLink="/info/credits" detail>
+              <IonIcon icon={heartOutline} size="small" slot="start" />
+              Credits
+            </IonItem>
+          </div>
+        </IonList>
+      </Main>
+    </Page>
   );
 };
 

2e6faad1f1543852876ae763ca47e35ef9991f47
vilius-stankaitis <vilius@stankaitis.com>
2022-10-10 13:30:08 +0300

    Add missing species image

--- src/common/data/species/photos/sickle/index.ts
+++ src/common/data/species/photos/sickle/index.ts
@@ -1,5 +1,6 @@
 import img1 from './sickle-1.jpg';
 import img2 from './sickle-2.jpg';
 import img3 from './sickle-3.jpg';
+import img4 from './sickle-4.jpg';
 
-export default [img1, img2, img3];
+export default [img1, img2, img3, img4];

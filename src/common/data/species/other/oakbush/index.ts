9efcea84e4f715c5af08bbe83026cd620d123b21
vilius-stankaitis <vilius@stankaitis.com>
2023-01-24 15:49:15 +0200

    #57 Fix missing lifechart diagram

--- src/common/data/species/other/oakbush/index.ts
+++ src/common/data/species/other/oakbush/index.ts
@@ -4,6 +4,7 @@ import sound from './oakbush_sound.mp3';
 import sonogram from './oakbush_sonogram.svg';
 import backgroundThumbnail from './thumbnail_background.png';
 import descriptionImg from './oakbush_descriptionImg.jpg';
+import lifecycle from './oakbush_lifecycle.jpg';
 
 export default {
   thumbnail,
@@ -12,4 +13,5 @@ export default {
   descriptionImg,
   sound,
   sonogram,
+  lifecycle,
 };
